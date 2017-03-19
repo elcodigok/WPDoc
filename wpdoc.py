4bc6fd94de341c301a801e413bfa427dc8b4a20b
Daniel Maldonado <daniel_5502@yahoo.com.ar>
2017-03-18 22:41:54 -0300

    New options.

---
+++ wpdoc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from optparse import OptionParser
+from optparse import OptionGroup
+
+def cmdLineParser():
+	"""Implementation to WPDoc."""
+	
+	usage = "usage: python %prog [options]"
+	version = 0.1
+	parser = OptionParser(usage, version=version)
+	parser.add_option("-v", "--verbose", action="store_true",
+					  dest="verbose", default=False,
+					  help="Verbose.")
+	target = OptionGroup(parser, "Target", "At least one of these options has to be provided to define the Directory Work.")
+	target.add_option("-d", "--dir", dest="path", help="**REQUIRED** -"
+					  " Working Directory", metavar="DIRECTORY")
+
+	parser.add_option_group(target)
+
+	(options, args) = parser.parse_args()
+
+	if options.path is None:
+		parser.print_help()
+		sys.exit()
+
+if __name__ == "__main__":
+	cmdLineParser()
